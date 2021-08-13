77af9d0332c2ebbcb495eab5e08dc4c62cbb68e1
ashdolldesert <feather.wizard@gmail.com>
2021-08-13 21:49:53 +0800

    ngwebap - 1. create ap

---
+++ ngwebap/ap/src/main/java/com/nordbank/ngwebap/ap/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.nordbank.ngwebap.ap.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "{status: '0', message: '', result: {}}";
+    }
+}
---
+++ ngwebap/ap/src/test/java/com/nordbank/ngwebap/ap/ApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nordbank.ngwebap.ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
