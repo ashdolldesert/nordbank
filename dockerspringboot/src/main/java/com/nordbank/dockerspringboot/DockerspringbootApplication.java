37f332f1f91ca91190151b4b769abd5993f92b76
ashdolldesert <feather.wizard@gmail.com>
2021-08-18 09:41:36 +0800

    dockerspringboot - cancel SpringBuiltInEventsListener

--- dockerspringboot/src/main/java/com/nordbank/dockerspringboot/DockerspringbootApplication.java
+++ dockerspringboot/src/main/java/com/nordbank/dockerspringboot/DockerspringbootApplication.java
@@ -2,22 +2,28 @@ package com.nordbank.dockerspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerspringbootApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DockerspringbootApplication.class);
-
-	}
+public class DockerspringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(DockerspringbootApplication.class);
-		// springApplication.addListeners(new SpringBuiltInEventsListener());
-		springApplication.run(args);
+		SpringApplication.run(DockerspringbootApplication.class, args);
 	}
 
 }
+// @SpringBootApplication
+// public class DockerspringbootApplication extends SpringBootServletInitializer {
+
+// 	@Override
+// 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+// 		return application.sources(DockerspringbootApplication.class);
+
+// 	}
+
+// 	public static void main(String[] args) {
+// 		SpringApplication springApplication = new SpringApplication(DockerspringbootApplication.class);
+// 		springApplication.addListeners(new SpringBuiltInEventsListener());
+// 		springApplication.run(args);
+// 	}
+
+// }
