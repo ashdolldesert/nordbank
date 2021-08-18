35b84df90cb768f9c6993bf786efc7782e826eb7
ashdolldesert <feather.wizard@gmail.com>
2021-08-18 14:18:58 +0800

    ngwebap web - update CommonController

--- ngwebap/web/src/main/java/com/nordbank/ngwebap/web/controller/CommonController.java
+++ ngwebap/web/src/main/java/com/nordbank/ngwebap/web/controller/CommonController.java
@@ -1,17 +1,21 @@
 package com.nordbank.ngwebap.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommonController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     
+    @RequestMapping("/**")
+    public String all(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String reqBody) {
+        String uri = request.getRequestURI();
+        return uri;
+    }
 }
