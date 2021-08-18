efaf3378ce9ad9823f30ea64a933e543f7fedbd9
ashdolldesert <feather.wizard@gmail.com>
2021-08-18 14:55:56 +0800

    ngwebap web - update CommonController

--- ngwebap/web/src/main/java/com/nordbank/ngwebap/web/controller/CommonController.java
+++ ngwebap/web/src/main/java/com/nordbank/ngwebap/web/controller/CommonController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UrlPathHelper;
 
 @RestController
 public class CommonController {
@@ -15,7 +16,37 @@ public class CommonController {
     
     @RequestMapping("/**")
     public String all(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String reqBody) {
-        String uri = request.getRequestURI();
-        return uri;
+        String path = new UrlPathHelper().getPathWithinApplication(request);
+        String method = request.getMethod();
+        boolean isGET = method.equals("GET");
+        boolean isPOST = method.equals("POST");
+        boolean isPUT = method.equals("PUT");
+        boolean isHEAD = method.equals("HEAD");
+        boolean isDELETE = method.equals("DELETE");
+        boolean isPATCH = method.equals("PATCH");
+        boolean isOPTIONS = method.equals("OPTIONS");
+        if (path.startsWith("/api")) {
+            return api(request, response, reqBody, method, path);
+        } else if (path.startsWith("/publicApi")) {
+            return publicApi(request, response, reqBody, method, path);
+        } else {
+            return path;
+        }
+    }
+
+    /**
+     * @RequestMapping("/api")
+     */
+    public String api(HttpServletRequest request, HttpServletResponse response, String reqBody, String method, String path) {
+        // TODO        
+        return "api";
+    }
+
+    /**
+     * @RequestMapping("/publicApi")
+     */
+    public String publicApi(HttpServletRequest request, HttpServletResponse response, String reqBody, String method, String path) {
+        // TODO
+        return "publicApi";
     }
 }
