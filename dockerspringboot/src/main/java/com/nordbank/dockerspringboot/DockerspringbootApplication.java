04a352dc89dbea2841093c5fd09f1c84345141a4
ashdolldesert <feather.wizard@gmail.com>
2021-08-18 09:19:49 +0800

    dockerspringboot - extends SpringBootServletInitializer and addListeners SpringBuiltInEventsListener

--- dockerspringboot/src/main/java/com/nordbank/dockerspringboot/DockerspringbootApplication.java
+++ dockerspringboot/src/main/java/com/nordbank/dockerspringboot/DockerspringbootApplication.java
@@ -2,12 +2,22 @@ package com.nordbank.dockerspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerspringbootApplication {
+public class DockerspringbootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DockerspringbootApplication.class);
+
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DockerspringbootApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(DockerspringbootApplication.class);
+		springApplication.addListeners(new SpringBuiltInEventsListener());
+		springApplication.run(args);
 	}
 
 }
