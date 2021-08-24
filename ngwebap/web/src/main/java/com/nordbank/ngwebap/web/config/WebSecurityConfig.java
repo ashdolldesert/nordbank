1d702b4ec1c3c23fd96d42459ceb346750f1edf3
charles.yen <charles.yen@frog-jump.com>
2021-08-25 00:34:24 +0800

    ngwebap web - add security settings

--- ngwebap/web/src/main/java/com/nordbank/ngwebap/web/config/WebSecurityConfig.java
+++ ngwebap/web/src/main/java/com/nordbank/ngwebap/web/config/WebSecurityConfig.java
@@ -1,13 +1,69 @@
 package com.nordbank.ngwebap.web.config;
 
+import com.kgi.eopend3.web.jwt.CustomAuthenticationProvider;
+import com.kgi.eopend3.web.jwt.JWTAuthenticationFilter;
+import com.kgi.eopend3.web.jwt.JWTLoginFilter;
+import com.kgi.eopend3.web.jwt.JwtAuthenticationEntryPoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and().csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .addFilterBefore(new JWTLoginFilter("/api/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(new CustomAuthenticationProvider(appContext));
+    }
+
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+        web.ignoring().antMatchers("/publicApi/**").antMatchers("/front/**");
+
     }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
+
+// @Configuration
+// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//     @Override
+//     public void configure(WebSecurity web) throws Exception {
+//         web.ignoring().antMatchers("/**");
+//     }
+// }
