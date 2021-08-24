3d59dabaa4f74f381a3c9a3052b79770afa75b0a
charles.yen <charles.yen@frog-jump.com>
2021-08-25 01:56:27 +0800

    ngwebap common - update WebResult

--- ngwebap/common/src/main/java/com/nordbank/ngwebap/common/dto/WebResult.java
+++ ngwebap/common/src/main/java/com/nordbank/ngwebap/common/dto/WebResult.java
@@ -9,7 +9,49 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 public class WebResult {
+    
     Integer status;
     String message;
     Object result;
+
+    @Override
+    public String toString() {
+        return new GsonBuilder().serializeNulls().disableHtmlEscaping().create().toJson(this);
+        //return new Gson().toJson(this);
+    }
+
+    public static String GetResultString(Integer status, String message, Object result) {
+
+        //return new WebResult(status, message, result).toString();
+        String resultString = new WebResult(status, message, result).toString();
+        
+        // if(ESAPI.validator().isValidInput("WebResult", resultString, "Space", Integer.MAX_VALUE, false , false)){
+            return resultString;
+        // }else{
+        //     return new WebResult(99, "Invalid Result", null).toString();
+        // }
+    }
+    public static String GetResultString(Integer status, String message) {
+        return GetResultString(status, message, null);
+    }
+
+    public static String GetSuccessResult(Object result) {
+        // if(result==null){
+            result = "";
+        // }
+        return GetResultString(0, "成功", result);
+    }
+    public static String GetSuccessResult() {
+        return GetSuccessResult(null);
+    }
+
+    public static String GetFailResult(){
+        return GetResultString(9, "Invalid Input", null);
+    }
+    public static String GetFailResult(String msg){
+        return GetResultString(9, msg, null);
+    }
+    public static String GetSystemFailResult(){
+        return GetResultString(99, "系統錯誤", null);
+    }
 }
