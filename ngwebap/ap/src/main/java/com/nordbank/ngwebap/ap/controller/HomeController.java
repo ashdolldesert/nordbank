e4066fb95f0c210786f50f4552088e76087e537c
ashdolldesert <feather.wizard@gmail.com>
2021-09-02 00:32:49 +0800

    ngwebap web - DEBUG ap dummy login

--- ngwebap/ap/src/main/java/com/nordbank/ngwebap/ap/controller/HomeController.java
+++ ngwebap/ap/src/main/java/com/nordbank/ngwebap/ap/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.nordbank.ngwebap.ap.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.nordbank.ngwebap.common.dto.WebResult;
+import com.nordbank.ngwebap.common.dto.response.CaseResp;
+import com.nordbank.ngwebap.common.dto.response.LoginResp;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
@@ -13,8 +17,15 @@ public class HomeController {
         return "{status: '0', message: '', result: {}}";
     }
 
-    @GetMapping("/login")
+    @RequestMapping("/login")
     public String login() {
-        return "{status: '0', message: 'login', result: {}}";
+        CaseResp resp = new CaseResp();
+        LoginResp loginResp = new LoginResp();
+        loginResp.setUniqId("LOAN0000000000000");
+        loginResp.setIdno("A123123123");
+        loginResp.setIpAddress("127.0.0.1");
+        resp.setLoginResp(loginResp);
+        return WebResult.GetResultString(0, "成功", resp);
+        // return "{status: '0', message: 'login', result: {}}";
     }
 }
