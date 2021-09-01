72284b0ea5e30b56bde383bd41d3ef2707c06937
ashdolldesert <feather.wizard@gmail.com>
2021-09-02 00:33:15 +0800

    ngwebap web - DEBUG key length

--- ngwebap/common/src/main/java/com/nordbank/ngwebap/common/SystemConst.java
+++ ngwebap/common/src/main/java/com/nordbank/ngwebap/common/SystemConst.java
@@ -3,7 +3,7 @@ package com.nordbank.ngwebap.common;
 public class SystemConst {
     /** jwt effective time: 5 days */
     public static final long EXPIRATIONTIME = 432_000_000;
-    public static final byte[] key = "nordbank2021charles".getBytes();
+    public static final byte[] key = "nordbank2021char".getBytes();
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADER_STRING = "Authorization";
 }
